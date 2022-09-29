1385888b967e417e182684aaa12c8374394034ca
Kaseax <80108547+Unerheblich@users.noreply.github.com>
2022-09-29 19:11:32 +0200

    Add Languages to Scoreboard

--- src/main/kotlin/de/variamc/lobby/events/JoinPlayerReset.kt
+++ src/main/kotlin/de/variamc/lobby/events/JoinPlayerReset.kt
@@ -1,5 +1,6 @@
 package de.variamc.lobby.events
 
+import de.variamc.lobby.functionality.LobbyItems
 import net.axay.kspigot.extensions.bukkit.feedSaturate
 import net.axay.kspigot.extensions.bukkit.heal
 import org.bukkit.entity.Player
@@ -13,6 +14,7 @@ object JoinPlayerReset {
         player.feedSaturate()
         player.heal()
         player.inventory.clear()
+        LobbyItems.givePlayer(player)
         for (potionEffect in player.activePotionEffects) {
             player.removePotionEffect(potionEffect.type)
         }
--- src/main/kotlin/de/variamc/lobby/utils/MainScoreboard.kt
+++ src/main/kotlin/de/variamc/lobby/utils/MainScoreboard.kt
@@ -3,25 +3,36 @@ package de.variamc.lobby.utils
 import de.variamc.lobby.LOBBY_PREFIX
 import io.github.variamc.coreapi.coins.CoinAPI
 import io.github.variamc.coreapi.scoreboard.ScoreboardBuilder
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.apache.commons.lang.LocaleUtils
 import org.bukkit.entity.Player
+import java.util.*
 
 /**
  * Class created by Kaseax on 2022
  */
 class MainScoreboard(player: Player?) : ScoreboardBuilder(player, "§eVaria§6MC") {
 
+    val resourceBundle: ResourceBundle = ResourceBundle.getBundle("lang.lobby", LocaleUtils.toLocale("en_US"))
+    //val resourceBundle: ResourceBundle = ResourceBundle.getBundle("lang.lobby", LocaleUtils.toLocale(LanguageAPI.getLanguage(player.getUniqueId().toString())
+    var mm = MiniMessage.miniMessage();
+
     override fun createScoreboard() {
         setScore("§e", 15);
-        setScore("§6§lCoins", 14);
+        val coins: Component = mm.deserialize(resourceBundle.getString("scoreboard.coins"))
+        setScore("§6§l$coins", 14);
         setScore("§7" + CoinAPI.getCoins(player.uniqueId.toString()), 13);
         setScore("§b", 12);
-        setScore("§6§lDein Rang", 11);
+        val rank: Component = mm.deserialize(resourceBundle.getString("scoreboard.rank"))
+        setScore("§6§l$rank", 11);
         //TODO: Add CloudNet-API
         setScore("§7" + "Spieler", 10);
         setScore("§8", 9);
         //TODO: Add Real playtime
-        setScore("§6§lSpielzeit", 8);
-        setScore("§7" + "1 Stunde 15 Minuten", 7);
+        val playTime: Component = mm.deserialize(resourceBundle.getString("scoreboard.playtime"))
+        setScore("§6§l$playTime", 8);
+        setScore("§7" + "1 Hour 15 minutes", 7);
         setScore("§c", 6);
     }
     override fun update() {}
